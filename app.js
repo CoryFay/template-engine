0ab0f48bf81fce949c3028fe228de175d3971cdc
Cory <coryfay97@gmail.com>
2020-12-17 13:29:52 -0800

    finished prompts and attached 'writeFile' logic.

--- app.js
+++ app.js
@@ -9,11 +9,175 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { endianness } = require("os");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
+const output = [];
+const starterQuestions = [
+  {
+    type: 'confirm',
+    message: 'Would you like to add a team member?',
+    name: 'addMore',
+    default: false
+  },
+  {
+    type: 'list',
+    message: 'Select a role: ',
+    choices: ['Manager', 'Engineer', 'Intern', 'None'],
+    default: 'None',
+    name: 'role'
+  }
+]
+const managerQuestions = [
+  {
+    type: 'list',
+    message: 'Reconfirm the role of the team member: ',
+    choices: ['Manager', 'Engineer', 'Intern', 'None'],
+    name: 'role',
+    default: 'Manager'
+  },
+  {
+    type: 'input',
+    message: 'Enter the team member name: ',
+    name: 'tmName',
+    default: 'Name'
+  },
+  {
+    type: 'number',
+    message: 'Enter the team member ID: ',
+    name: 'tmID',
+    default: 00
+  },
+  {
+    type: 'input',
+    message: 'Enter the team member email: ',
+    name: 'tmEmail',
+    default: 'tm@email.com'
+  },
+  {
+    type: 'number',
+    message: 'Enter the office number: ',
+    name: 'tmOfficeNum',
+    default: 00
+  }
+]
+const engineerQuestions = [
+  {
+    type: 'list',
+    message: 'Reconfirm the role of the team member: ',
+    choices: ['Manager', 'Engineer', 'Intern', 'None'],
+    name: 'role',
+    default: 'Engineer'
+  },
+  {
+    type: 'input',
+    message: 'Enter the team member name: ',
+    name: 'tmName',
+    default: 'Name'
+  },
+  {
+    type: 'number',
+    message: 'Enter the team member ID: ',
+    name: 'tmID',
+    default: 00
+  },
+  {
+    type: 'input',
+    message: 'Enter the team member email: ',
+    name: 'tmEmail',
+    default: 'tm@email.com'
+  },
+  {
+    type: 'input',
+    message: 'Enter the github username: ',
+    name: 'tmGithub',
+    default: 'tmGithub00'
+  }
+]
+const internQuestions = [
+  {
+    type: 'list',
+    message: 'Reconfirm the role of the team member: ',
+    choices: ['Manager', 'Engineer', 'Intern', 'None'],
+    name: 'role',
+    default: 'Intern'
+  },
+  {
+    type: 'input',
+    message: 'Enter the team member name: ',
+    name: 'tmName',
+    default: 'Name'
+  },
+  {
+    type: 'number',
+    message: 'Enter the team member ID: ',
+    name: 'tmID',
+    default: 00
+  },
+  {
+    type: 'input',
+    message: 'Enter the team member email: ',
+    name: 'tmEmail',
+    default: 'tm@email.com'
+  },
+  {
+    type: 'input',
+    message: 'Enter the school they attend: ',
+    name: 'tmSchool',
+    default: 'University'
+  }
+]
+function start(){
+  inquirer.prompt(starterQuestions).then((answers) =>{
+    if (answers.addMore === true && answers.role === 'Manager'){
+      manager();
+    }else if(answers.addMore === true && answers.role === 'Engineer'){
+      engineer();
+    }else if(answers.addMore === true && answers.role === 'Intern'){
+      intern();
+    }else{
+      end();
+    }
+  })
+  
+}
+function manager(){
+  inquirer.prompt(managerQuestions).then((answers) => {
+    const employee = new Manager(answers.tmName, answers.tmID, answers.tmEmail, answers.tmOfficeNum);
+    output.push(employee);
+    start();
+  })
+}
+function engineer(){
+  inquirer.prompt(engineerQuestions).then((answers) => {
+    const employee = new Engineer(answers.tmName, answers.tmID, answers.tmEmail, answers.tmGithub);
+    output.push(employee);
+    start();
+  })
+}
+function intern(){
+  inquirer.prompt(internQuestions).then((answers) => {
+    const employee = new Intern(answers.tmName, answers.tmID, answers.tmEmail, answers.tmSchool);
+    output.push(employee);
+    start();
+  })
+}
+function end(){
+  // console.log('You are all set! Here is your team: ');
+  // console.log('-----------------------------------');
+  // console.log(output);
+  const renderPage = render(output);
+  fs.writeFile(outputPath, renderPage, function(err) {
+    if (err){
+      return console.log(err)
+    }
+    console.log('Your page has been generated in the "output" folder.')
+  })
+}
+start();
+  
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
